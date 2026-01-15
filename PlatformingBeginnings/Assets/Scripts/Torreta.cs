42fc774cf25800bc3e684ff7a88aba79a3210099
Lufku <adrivm133@gmail.com>
2026-01-15 15:51:40 +0100

    Class progress

---
+++ PlatformingBeginnings/Assets/Scripts/Torreta.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Torreta : MonoBehaviour
+{
+    [Header("Rango de Detección")]
+    [SerializeField] float RangoVision;
+    [SerializeField] Transform player;
+
+    bool jugadorDetectado = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
