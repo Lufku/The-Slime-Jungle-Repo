4af5e19543c28d8a2bbdfa6abffd04793b414456
Lufku <adrivm133@gmail.com>
2026-01-15 17:20:58 +0100

    Class Progress i lost count of the number

--- PlatformingBeginnings/Assets/Scripts/MovimientoBala.cs
+++ PlatformingBeginnings/Assets/Scripts/MovimientoBala.cs
@@ -4,20 +4,24 @@ public class MovimientoBala : MonoBehaviour
 {
     [SerializeField] float speed;
     Rigidbody2D rbbala;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rbbala = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject, 3f);
     }
 
     void FixedUpdate()
     {
         rbbala.MovePosition(transform.position + transform.right * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            Destroy(gameObject);
+            Debug.Log("He sido golpeado por una bala");
+        }
     }
 }
--- PlatformingBeginnings/Assets/Scripts/Torreta.cs
+++ PlatformingBeginnings/Assets/Scripts/Torreta.cs
@@ -7,37 +7,48 @@ public class Torreta : MonoBehaviour
     [SerializeField] Transform player;
 
     bool jugadorDetectado = false;
+    bool jugadorenRango;
 
-    void Start()
-    {
-        
-    }
+    [Header("Disparo de la Torreta")]
+    public GameObject bala;
+    public Transform spawn;
 
-    // Update is called once per frame
     void Update()
     {
         deteccion();
-        //Disparo();
+        Disparo();
+        
     }
 
-
-	void OnDrawGizmos()
-	{
-		Gizmos.color = Color.red;
-		Gizmos.DrawWireSphere(transform.position, RangoVision);
-	}
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, RangoVision);
+    }
 
     void deteccion()
     {
         float distancia = Vector2.Distance(transform.position, player.position);
+        jugadorenRango = distancia <= RangoVision;
 
-        if(distancia <= RangoVision && !jugadorDetectado){
-        jugadorDetectado = true;
-        Debug.Log("Jugador Detectado");
+        if (distancia <= RangoVision && !jugadorDetectado)
+        {
+            jugadorDetectado = true;
+            Debug.Log("Jugador Detectado");
         }
-        else if(distancia > RangoVision && jugadorDetectado){
+        else if (distancia > RangoVision && jugadorDetectado)
+        {
             jugadorDetectado = false;
             Debug.Log("Salí del rango");
+        }
+    }
+
+    void Disparo()
+    {
+        if (jugadorenRango && (Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0)))
+        {
+            Instantiate(bala, spawn.position, spawn.rotation);
+            Debug.Log("Bala Disparada");
         }
     }
 }
